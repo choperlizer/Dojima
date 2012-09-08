f8e8b3283f4577baf14a19738fcaa98df5620512
Emery Hemingway <tulpenmanie@emery.neomailbox.net>
2012-09-08 14:59:23 -0500

    Version bumped to 0.4.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='tulpenmanie',
-      version='0.3.1',
+      version='0.4.0',
       description='Graphical commodity market client.',
       url='https://github.com/3M3RY/tulpenmanie',
       author='Emery Hemingway',
