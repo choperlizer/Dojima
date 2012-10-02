2f69c3544cb91d4164c8593e469329435027db94
Emery Hemingway <tulpenmanie@emery.neomailbox.net>
2012-10-02 13:49:49 -0500

    mtgox API 1 fallback

--- tulpenmanie/exchange_modules/__init__.py
+++ tulpenmanie/exchange_modules/__init__.py
@@ -1,2 +1,8 @@
 # Add new modules here, so they get imported by *
-__all__ = ['bitstamp', 'btc-e', 'campbx', 'mtgox']
+__all__ = ['bitstamp', 'btc-e', 'campbx']
+
+try:
+    import socketIO_client
+    __all__.append('mtgox_streaming')
+except ImportError:
+    __all__.append('mtgox_api1')
